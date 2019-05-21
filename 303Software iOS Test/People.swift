405c871546e628a6960dceab4525e0d16e814288
Michael Jajou <jajoumic@msu.edu>
2019-05-21 15:53:52 -0400

    Create People Struct and began parsing data

---
+++ 303Software iOS Test/People.swift
@@ -0,0 +1,15 @@
+//
+//  People.swift
+//  303Software iOS Test
+//
+//  Created by Michael Jajou on 5/21/19.
+//  Copyright © 2019 Apptomistic. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let fname: String
+    let lname: String
+    let city: String
+}
--- 303Software iOS Test/ViewController.swift
+++ 303Software iOS Test/ViewController.swift
@@ -13,30 +13,60 @@ class ViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     // Temporary array while setting up table view
-    var data: [String] = []
+    var data: [Person] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
+    }
+    
+    func getData() {
         if let url = URL(string: "http://www.filltext.com/?rows=100&fname=%7BfirstName%7D&lname=%7BlastName%7D&city=%7Bcity%7D&pretty=true") {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
                 guard let data = data else { return }
-                print(String(data: data, encoding: .utf8)!)
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [Any] {
+                    for object in json {
+                        if let item = object as? [String: Any] {
+                            self.addPerson(dict: item)
+                        }
+                    }
+                }
             }
-            
+            tableView.reloadData()
             task.resume()
         }
+    }
+    
+    func addPerson(dict: [String: Any]) {
+        guard let fname = dict["fname"] as? String else { return }
+        guard let lname = dict["lname"] as? String else { return }
+        guard let city = dict["city"] as? String else { return }
+        
+        let person = Person(fname: fname, lname: lname, city: city)
+        data.append(person)
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath)
-        cell.textLabel?.text = "Hello"
+        let nameLabel = cell.viewWithTag(0) as! UILabel
+        let cityLabel = cell.viewWithTag(1) as! UILabel
+        let person = data[indexPath.row]
+        let name = person.fname + person.lname
+        let city = person.city
+        nameLabel.text = name
+        cityLabel.text = city
         return cell
     }
     
