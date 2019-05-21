b91bcf26e0f18f985dda7127dc88c655a1db226d
Michael Jajou <jajoumic@msu.edu>
2019-05-21 17:09:25 -0400

    Created view with tags in TableViewCell. Parsed the data, and displayed it into the table view.

--- 303Software iOS Test/ViewController.swift
+++ 303Software iOS Test/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  303Software iOS Test
-//
-//  Created by Michael Jajou on 5/21/19.
-//  Copyright © 2019 Apptomistic. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
-    // Temporary array while setting up table view
+    // Array while setting up table view
     var data: [Person] = []
     
     
@@ -22,32 +14,53 @@ class ViewController: UIViewController {
     }
     
     func getData() {
+        // If we can convert the string to a URL, then proceed
         if let url = URL(string: "http://www.filltext.com/?rows=100&fname=%7BfirstName%7D&lname=%7BlastName%7D&city=%7Bcity%7D&pretty=true") {
+            
+            // Create a dispatch group in order to let the table view know when to reload its data
+            let myGroup = DispatchGroup()
+            
+            // Attempt to get JSON data from URL Session
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print(error!.localizedDescription)
                     return
                 }
                 
+                // Unwrap data in guard statement
                 guard let data = data else { return }
+                
+                // Convert the data of people into an array
                 if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [Any] {
+                    
+                    // Iterate through each object, and construct a person object
                     for object in json {
+                        myGroup.enter()
                         if let item = object as? [String: Any] {
                             self.addPerson(dict: item)
+                            myGroup.leave()
+                            // enter and exit dispatch group
                         }
+                    }
+                    
+                    // Once we have added all people from data to array, reload table view
+                    myGroup.notify(queue: .main) {
+                        self.tableView.reloadData()
                     }
                 }
             }
-            tableView.reloadData()
             task.resume()
         }
     }
     
     func addPerson(dict: [String: Any]) {
+        
+        // Attempt to fetch names and city from dictionary
         guard let fname = dict["fname"] as? String else { return }
         guard let lname = dict["lname"] as? String else { return }
         guard let city = dict["city"] as? String else { return }
         
+        // Construct the person and add it to data
         let person = Person(fname: fname, lname: lname, city: city)
         data.append(person)
     }
@@ -60,13 +73,18 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath)
-        let nameLabel = cell.viewWithTag(0) as! UILabel
-        let cityLabel = cell.viewWithTag(1) as! UILabel
+        
+        // Get the proper labels from the cells view using tags
+        let nameLabel = cell.viewWithTag(1) as! UILabel
+        let cityLabel = cell.viewWithTag(2) as! UILabel
+        
+        // Fetch names and city from data
         let person = data[indexPath.row]
-        let name = person.fname + person.lname
+        let name = person.fname + " " + person.lname
         let city = person.city
         nameLabel.text = name
         cityLabel.text = city
+        
         return cell
     }
     
